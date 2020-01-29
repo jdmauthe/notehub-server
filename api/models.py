d6afbe8e89b06c380254bd04f11f45ff01cb5d4c
jdmauthe <mauthejd@gmail.com>
2020-01-29 01:21:16 -0800

    added note model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Note(models.Model):
+    author_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    university = models.CharField(max_length=50)
+    course = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
