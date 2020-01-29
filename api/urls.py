dae23ffeeef1639ce61bb20de0f19a53fdfa4ace
jdmauthe <mauthejd@gmail.com>
2020-01-29 14:42:13 -0800

    created urls for Note and NoteFile

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from rest_framework.authtoken import views
-from .views import UserView
+from .views import UserView, NoteView, NoteDetailView, NoteFileView, NoteFileDetailView
 
 urlpatterns = [
-    path('login/', views.obtain_auth_token),
-    path('user/', UserView.as_view()),
+    path("login/", views.obtain_auth_token),
+    path("user/", UserView.as_view()),
+    path("notes/", NoteView.as_view()),
+    path("notes/<int:pk>/", NoteDetailView.as_view()),
+    path("notes/<int:note_id>/files", NoteFileView.as_view()),
+    path("notes/<int:note_id>/files/<int:index>", NoteFileDetailView.as_view()),
 ]
