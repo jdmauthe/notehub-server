b436d36df4013129b9f94f1f4a82e90de706d19f
jdmauthe <mauthejd@gmail.com>
2020-02-23 16:58:25 -0800

    allow admin ability for models

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
+from .models import (
+    Note,
+    NoteFile,
+    Rating,
+    Comment,
+    Group,
+    NoteReport,
+    CommentReport,
+)
 # Register your models here.
+
+admin.site.register(Note)
+admin.site.register(NoteFile)
+admin.site.register(Rating)
+admin.site.register(Comment)
+admin.site.register(Group)
+admin.site.register(NoteReport)
+admin.site.register(CommentReport)
