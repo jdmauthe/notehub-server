7f8595272f93d790a5da074241971b3464929995
jdmauthe <mauthejd@gmail.com>
2020-02-28 07:19:19 -0800

    added username query for users view

--- api/views.py
+++ api/views.py
@@ -49,8 +49,14 @@ from .serializers import (
 # Create your views here.
 class UserView(mixins.CreateModelMixin, mixins.ListModelMixin, generics.GenericAPIView):
     permission_classes = (permissions.AllowAny,)
-    queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    def get_queryset(self):
+        queryset = User.objects.all()
+        username = self.request.query_params.get("username", None)
+        if username is not None:
+            queryset = queryset.filter(username=username)
+        return queryset
 
     def get(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
